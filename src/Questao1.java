ccba58cd0ba7cd3ae434d1a5e6b4d678b386daa9
RODRIGO NATHAN OLIVEIRA <09984799697>
2022-08-24 20:43:55 -0300

    codigo inicial

---
+++ src/Questao1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Questao1 {
+    public static void main(String[] args) throws Exception {
+ 
+        Scanner scanner = new Scanner(System.in);
+
+        int prim= 0, seg = 1;
+
+        while(prim != seg){
+            System.out.println("Digite dois numeros: ");
+            prim = scanner.nextInt();
+            seg = scanner.nextInt();
+
+            if (prim > seg){
+                System.out.println("Decrescente");
+            } else if (prim < seg){
+                System.out.println("Crescente");
+            }
+
+        }
+
+
+
+
+
+        scanner.close();
+
+    }
+}
