56e97e689b902aa5513b9ba6d3ade63a746301ee
RODRIGO NATHAN OLIVEIRA <09984799697>
2022-08-26 21:50:11 -0300

    pre codigo

--- src/Questao9.java
+++ src/Questao9.java
@@ -20,9 +20,24 @@ public class Questao9 {
             menor = n;
         }
 
-        cont =((menor - maior)*-1)/2;
+        cont =((menor - maior +1)/2)*-1;
+       
+        if (cont !=0){
+        if (menor%2!=0){
+            for (int i = 1; i <= cont; i++) {
+                result = result + menor + 2*i;
+            }
+        } else {
+            menor = menor - 1;
+            for (int i = 1; i <= cont; i++) {
+                result = result + menor + 2*i;
+            }
+        }
+    }
 
 
         System.out.println("Soma dos impares: " + result);
+
+        scanner.close();
     }
 }
